97509990370f196ec5415416a6b7362553487fe3
ClaySamuelC <claysamuelc@gmail.com>
2022-11-17 22:21:19 -0800

    Fix dependency issues.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {tables} from './dict.js';
+import {rollTable} from './roller.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
